bbcf563ac398a7af7b17e19f2b653be260a6d3da
KamuiRyu <koushirogamer@gmail.com>
2024-11-01 04:20:17 +0000

    :wrench: chore: Configurações de alguns arquivos e modificações em estrutura

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
+const path = require('path');
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname),
+      '@styles': path.resolve(__dirname, 'src/styles'),
+      '@fonts': path.resolve(__dirname, 'src/fonts'),
+      '@components': path.resolve(__dirname, 'src/components'),
+      '@utils': path.resolve(__dirname, 'src/utils'),
+      '@core-api': path.resolve(__dirname, 'src/core-api')
+    };
+    return config;
+  }
 };
 
 export default nextConfig;
